149efc2d933cf592d869fb31846cdca1dcb90b65
Darwin Perdomo <ZeroBl@hotmail.com>
2022-06-12 20:11:58 -0400

    feat(services): Added getGifs servica and private variables

---
+++ src/services/getWeather.jsx
@@ -0,0 +1,22 @@
+import { BASE_URL, API_KEY } from './settings'
+
+const extractData = (apiResponse) => {
+  if (!apiResponse?.error) {
+    console.log(apiResponse)
+    const { name, country } = apiResponse.location
+    const { temp_c, humidity, vis_km, is_day, condition } = apiResponse.current
+
+    return { name, country, temp_c, humidity, vis_km, is_day, condition }
+  }
+}
+
+const getWeather = ({ city }) => {
+  const API = `${BASE_URL}current.json?key=${API_KEY}q=${city}&aqi=no`
+
+  return window
+    .fetch(API)
+    .then((res) => res.json())
+    .then(extractData)
+}
+
+export default getWeather
---
+++ src/services/settings.js
@@ -0,0 +1,2 @@
+export const BASE_URL = 'http://api.weatherapi.com/v1/'
+export const API_KEY = 'a4b97ced9b7542689a9175331221006&'
