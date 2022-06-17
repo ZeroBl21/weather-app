e625d8746783359196045180ae4c7a1c4ec32fd7
Darwin Perdomo <ZeroBl@hotmail.com>
2022-06-17 19:32:39 -0400

    Style(Card): Card Layout has been changed

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,26 +1,43 @@
 import React from 'react'
 import './Card.css'
 
-const Card = ({ name, country, temp_c, humidity, vis_km, condition }) => {
+const Card = ({
+  name,
+  country,
+  temp_c,
+  humidity,
+  vis_km,
+  condition,
+  localtime,
+  feelslike_c,
+}) => {
   return (
-    <article className='card'>
-      <section className='card__section'>
-        <article>
-          <h2>{temp_c} °C</h2>
-          <p>{condition?.text}</p>
-        </article>
-        <div className='card__divider' />
-        <article>
-          <h2>{country}</h2>
-          <p>{name}</p>
-        </article>
-      </section>
+    <>
+      <header className='card__header'>
+        <p>
+          {country}, {name}
+        </p>
+        <h2 className='card__temp'>{temp_c} °C</h2>
+        <p className='card__condition'>{condition?.text}</p>
+      </header>
+      <article className='card'>
+        <section className='card__section'>
+          <article>
+            <p>Feelslike {feelslike_c} °C</p>
+            <p>Humidity {humidity}%</p>
+          </article>
+          <div className='card__divider' />
+          <article>
+            <p>Localtime</p>
+            <h2>{localtime?.split(' ')[1]}</h2>
+          </article>
+        </section>
 
-      <section className='card__section'>
-        <p>Humidity {humidity}%</p>
-        <p>{vis_km}km/h</p>
-      </section>
-    </article>
+        <section className='card__section'>
+          <p>{vis_km}km/h</p>
+        </section>
+      </article>
+    </>
   )
 }
 
